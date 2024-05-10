96d561f0b02fbcbcfc5e73955ba92037f2e11597
Erica Floreani <61478451+edflorea@users.noreply.github.com>
2024-05-10 11:23:07 -0600

    Starting mobile design

--- script.js
+++ script.js
@@ -8,8 +8,17 @@ const currentPageId = getCurrentPageId();
 
 mainContent.addEventListener("scroll", function () {
     var navLinks = document.querySelectorAll(".nav-link");
-    var earlyAccess = document.querySelector("#early-access");
+    var earlyAccessDesktop = document.querySelector(".early-access.desktop");
+    var earlyAccessMobile = document.querySelector(".early-access.mobile");
+    var mobileLogo = document.querySelector("#mobile-logo");
+    var mobileMenuLogo = document.querySelector("#mobile-menu-logo");
+    var mobileNav = document.querySelector("#mobile-nav");
+    var hamburgerMenu = document.querySelector("menu-icon");
+    var closeIcon = document.querySelector("close-icon");
+    var mobileNavFooter = document.querySelector("#mobile-nav-footer");
+
     var footer = document.querySelector("footer");
+    // var scrollBarStyle = document.querySelector("#scrollbar-style");
     var scrollPosition = mainContent.scrollTop;
 
     if (scrollPosition > 0.1 * screenHeight) {
@@ -17,8 +26,22 @@ mainContent.addEventListener("scroll", function () {
     } else {
         footer.classList.remove("scrolled");
     }
+    /* if (scrollPosition > 0.5 * screenHeight) {
+        scrollBarStyle.innerHTML = scrolledScrollBar;
+    } else {
+        scrollBarStyle.innerHTML = unscrolledScrollBar;
+    } */
     if (scrollPosition > 0.9 * screenHeight) {
-        earlyAccess.classList.add("scrolled");
+        earlyAccessDesktop.classList.add("scrolled");
+        earlyAccessMobile.classList.add("scrolled");
+        mobileNav.classList.add("scrolled");
+        hamburgerMenu.classList.add("scrolled");
+        closeIcon.classList.add("scrolled");
+        mobileNavFooter.classList.add("scrolled");
+
+        mobileLogo.setAttribute("src", "./assets/possibility_neuron_color.png");
+        mobileMenuLogo.setAttribute("src", "./assets/possibility_logo_rgb.png");
+
         for (i = 0; i < navLinks.length; i++) {
             if (navLinks[i].id == "t2s-link" && currentPageId == "t2s-link") {
                 navLinks[i].classList.add("current-page");
@@ -30,7 +53,16 @@ mainContent.addEventListener("scroll", function () {
             }
         }
     } else {
-        earlyAccess.classList.remove("scrolled");
+        earlyAccessDesktop.classList.remove("scrolled");
+        earlyAccessMobile.classList.remove("scrolled");
+        mobileNav.classList.remove("scrolled");
+        hamburgerMenu.classList.remove("scrolled");
+        closeIcon.classList.remove("scrolled");
+        mobileNavFooter.classList.remove("scrolled");
+
+        mobileLogo.setAttribute("src", "./assets/possibility_neuron_white.png");
+        mobileMenuLogo.setAttribute("src", "./assets/possibility_logo_white.png");
+
         for (i = 0; i < navLinks.length; i++) {
             if (navLinks[i].id == "t2s-link" && currentPageId == "t2s-link") {
                 navLinks[i].classList.remove("current-page");
@@ -80,3 +112,39 @@ if (headshots.length > 0) {
         });
     }
 }
+
+// -----------------------------------------------------------------------------------------------
+// Scrollbar
+// -----------------------------------------------------------------------------------------------
+const unscrolledScrollBar = `
+main::-webkit-scrollbar {
+    width: 8px;
+}
+
+main::-webkit-scrollbar-thumb {
+    background: #fff;
+    border-radius: 8px;
+    border: 2px solid #7f3f98;
+    height: 30px;
+}
+
+main::-webkit-scrollbar-track {
+    background-color: #7f3f98;
+}`;
+
+const scrolledScrollBar = `
+main::-webkit-scrollbar {
+    width: 8px;
+    transition: all 0.3s ease;
+}
+
+main::-webkit-scrollbar-thumb {
+    background: #7f3f98;
+    border-radius: 8px;
+    border: 2px solid #fff;
+    height: 30px;
+}
+
+main::-webkit-scrollbar-track {
+    background-color: #fff;
+}`;
